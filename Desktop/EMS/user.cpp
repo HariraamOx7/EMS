0e468a76a4e06ad0293fc568623aa7fbc7cc3ac2
HariraamOx7 <hariraam341@gmail.com>
2025-04-24 21:20:40 +0530

    basic student functionality

--- Desktop/EMS/user.cpp
+++ Desktop/EMS/user.cpp
@@ -1,12 +1,7 @@
 #include "User.h"
 
-// Initialize static member
-int User::nextUserId = 1;
+User::User(QWidget *parent) : QWidget(parent)
+{
 
-User::User(QWidget *parent) : QWidget(parent), userId(nextUserId++) {
-    // Initialize with default values
-    username = "";
-    password = "";
-    email = "";
 }
 
